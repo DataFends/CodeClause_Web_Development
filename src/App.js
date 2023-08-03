e2f0dfd376868815c71912a0795049aa0e60f095
Abdul Azeez <officiallyazeez@gmail.com>
2023-08-03 20:12:47 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FileUpload from './components/FileUpload/FileUpload';
+
+function App() {
+  return (
+    <div className='maincontainer'>
+        <FileUpload/>
+    </div>
+  );
+}
+
+export default App;
